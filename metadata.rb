344f456bf32ca8f7ebb728126a8c1c71cae491cf
Graham Christensen <gchristensen@nationalfield.org>
2012-09-18 15:57:22 -0400

    Create a way to add users into the ubuntu admin group; support password shadows in the databag.

---
+++ metadata.rb
@@ -0,0 +1,18 @@
+name "users-ext"
+description "Additional resources to create users among specific groups."
+maintainer "NationalField"
+maintainer_email "engineers@nationalfield.org"
+
+license "MIT"
+version "0.0.1"
+
+depends "users"
+depends "build-essential"
+supports "ubuntu", "= 12.04"
+
+long_description <<-EOH
+Adds support for:
+
+- Adding sets of users to the `admin` group. [Ubuntu's sudo group]
+EOH
+
